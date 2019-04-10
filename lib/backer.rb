cc412013ba93a06f053d6d794102f967013a7262
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-10 19:19:31 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,19 +1,11 @@
 class Backer
   
-  attr_accessor :project_name
+  attr_accessor :add_backer
   attr_reader :backed_projects, :name
   
   def initialize(name)
     @name = name
     @backed_projects = []
-  end
-  
-  def project_name=(name)
-    if (self.project.nil?)
-      self.project = Project.new(name)
-    else
-      self.project.name = name
-    end
   end
   
   def back_project(project)
